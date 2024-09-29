4e1ff7792c10354a84fe63fff3193e89bec9095b
Aaron Bittel <aaron@bittels.de>
2024-09-29 07:50:34 +0200

    clean up

--- cmd/testing_grounds/main.go
+++ cmd/testing_grounds/main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"time"
 	term_utils "tui/internal/term-utils"
 
 	"golang.org/x/term"
 )
 
 func main() {
-	time.Sleep(time.Second)
 	fd, oldState, err := term_utils.Start()
 	if err != nil {
 		panic(err)
 	}
 	defer term.Restore(fd, oldState)
 	defer term_utils.TearDown()
-
-	fixingRaceCondition()
 }
